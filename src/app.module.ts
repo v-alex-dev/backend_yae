507883c7fd2718f651a21ec41c602f0f814815ea
v-alex-dev <vensalex1991@gmail.com>
2025-01-03 19:01:40 +0100

    Add IngredientModule into app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
+import { IngredientModule } from './ingredient/ingredient.module';
 
 @Module({
-  imports: [],
+  imports: [IngredientModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
