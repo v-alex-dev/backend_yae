fc97e87b11c23cad99021f8b540876f8f873f774
v-alex-dev <vensalex1991@gmail.com>
2025-01-04 13:48:31 +0100

    init crud recipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { IngredientModule } from './ingredient/ingredient.module';
+import { RecipeModule } from './recipe/recipe.module';
 
 @Module({
-  imports: [IngredientModule],
+  imports: [IngredientModule, RecipeModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
