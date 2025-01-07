d56e54e6b804239dfa40e2a6be076ca8cf8ccb28
v-alex-dev <vensalex1991@gmail.com>
2025-01-07 17:25:19 +0100

    Add routing controller

--- src/recipe/recipe.controller.ts
+++ src/recipe/recipe.controller.ts
@@ -11,4 +11,31 @@ import { RecipeService } from './recipe.service';
 import { Recipe } from '@prisma/client';
 
 @Controller('recipe')
-export class RecipeController {}
+export class RecipeController {
+    constructor( private readonly recipeService: RecipeService) {}
+
+    @Get()
+    async getRecipes() : Promise<Recipe[]> {
+        return this.recipeService.recipes({});
+    }
+
+    @Get(':id')
+    async getRecipe(@Param('id') id: string) : Promise<Recipe> {
+        return this.recipeService.recipe({id: String(id)});
+    }
+
+    @Post()
+    async createRecipe(@Body() data: {name: string, description: string}) : Promise<Recipe> {
+        return this.recipeService.createRecipe(data);
+    }
+
+    @Put(':id')
+    async updateRecipe(@Param('id') id: string, @Body() data: {name: string, description: string}) : Promise<Recipe> {
+        return this.recipeService.updateRecipe({where: {id: String(id)}, data});
+    }
+
+    @Delete(':id')
+    async deleteRecipe(@Param('id') id: string) : Promise<Recipe> {
+        return this.recipeService.deleteRecipe({id: String(id)});
+    }
+}
