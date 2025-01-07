cec5bfade47665f90b58e265910433843ee00b36
v-alex-dev <vensalex1991@gmail.com>
2025-01-07 17:12:17 +0100

    Create CRUD basic

--- src/recipe/recipe.service.ts
+++ src/recipe/recipe.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, Recipe } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class RecipeService {}
+export class RecipeService {
+
+    constructor(private readonly prisma : PrismaService) {}
+
+    async recipe(recipeWhereUniqueInput: Prisma.RecipeWhereUniqueInput): Promise<Recipe | null> {
+        return this.prisma.recipe.findUnique({
+            where: recipeWhereUniqueInput,
+        });
+    }
+
+    async recipes(params: {
+        skip?: number;
+        take?: number;
+        cursor?: Prisma.RecipeWhereUniqueInput;
+        where?: Prisma.RecipeWhereInput;
+        orderBy?: Prisma.RecipeOrderByWithRelationInput;
+    }): Promise<Recipe[]> {
+        const { skip, take, cursor, where, orderBy } = params;
+        return this.prisma.recipe.findMany({
+            skip,
+            take,
+            cursor,
+            where,
+            orderBy,
+        });
+    }
+
+    async createRecipe(data: Prisma.RecipeCreateInput): Promise<Recipe> {
+
+        return this.prisma.recipe.create({
+            data,
+        });
+    }
+
+    async updateRecipe(params: {
+        where: Prisma.RecipeWhereUniqueInput;
+        data: Prisma.RecipeUpdateInput;
+    }): Promise<Recipe> {
+        const { where, data } = params;
+        return this.prisma.recipe.update({
+            data,
+            where,
+        });
+    }
+
+    async deleteRecipe(where: Prisma.RecipeWhereUniqueInput): Promise<Recipe> {
+        return this.prisma.recipe.delete({
+            where,
+        });
+    }
+
+}
