2980814c3c2491d9cf4d99aaeb52920bfa95ed9b
v-alex-dev <vensalex1991@gmail.com>
2025-01-07 17:25:03 +0100

    Add imports

--- src/recipe/recipe.controller.ts
+++ src/recipe/recipe.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Param,
+    Post,
+    Body,
+    Put,
+    Delete,
+  } from '@nestjs/common';
+import { RecipeService } from './recipe.service';
+import { Recipe } from '@prisma/client';
 
 @Controller('recipe')
 export class RecipeController {}
