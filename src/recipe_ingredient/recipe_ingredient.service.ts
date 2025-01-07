3a1b22d24eff0c18e619151b8d8995bb02ded406
v-alex-dev <vensalex1991@gmail.com>
2025-01-07 17:22:57 +0100

    Add create recipeIngredient

--- src/recipe_ingredient/recipe_ingredient.service.ts
+++ src/recipe_ingredient/recipe_ingredient.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, Recipe_ingredients } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class RecipeIngredientService {}
+export class RecipeIngredientService {
+    constructor(private readonly prisma : PrismaService) {}
+
+    async createRecipeIngredient(data: Prisma.Recipe_ingredientsCreateInput): Promise<Recipe_ingredients> { 
+        return this.prisma.recipe_ingredients.create({
+            data,
+        });
+    }
+}
